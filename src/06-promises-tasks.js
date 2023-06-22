0e53ccd1925aa861fa33161cc1a04f626c96c7c2
kotokatu <117390625+kotokatu@users.noreply.github.com>
2023-06-22 20:26:23 +0300

    add solution

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,12 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (isPositiveAnswer === true) resolve('Hooray!!! She said "Yes"!');
+    if (isPositiveAnswer === false) resolve('Oh no, she said "No".');
+    else reject(Error('Wrong parameter is passed! Ask her again.'));
+  });
 }
 
 
@@ -49,6 +53,7 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *
  */
 function processAllPromises(/* array */) {
+  // return new Promise((resolve, reject) => {
   throw new Error('Not implemented');
 }
 
